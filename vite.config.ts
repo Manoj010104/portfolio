b95c5bf1a85b67b4c886af3138d8af03124f62af
manoj010104 <mchirraboyina@gmail.com>
2025-02-28 17:36:35 +0530

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'https://github.com/Manoj010104/portfolio', // Replace 'portfolio' with your repository name
-  plugins: [react()],
-});
